a8f3c1c9f1fae0f3b8655888c98d82c022a985fc
Eyerusalem-Hailemariam <eyerusalem008@gmail.com>
2025-01-19 11:55:49 +0300

    working on payment card

--- Amazon/src/Pages/Payment/Payment.jsx
+++ Amazon/src/Pages/Payment/Payment.jsx
@@ -1,13 +1,22 @@
-import React, {useContext} from 'react'
+import React, {useContext, useState} from 'react'
 import LayOut from '../../Components/LayOut/LayOut'
 import './Payment.css'
 import { DataContext } from '../../Components/DataProvider/DataProvider'
 import ProductCard from '../../Components/Product/ProductCard'
+import {useStripe, useElements, CardElement} from '@stripe/react-stripe-js';
 
 function Payment() {
-
+  cosnt [error, setError] = useState(null);
   const [{user, basket}] = useContext(DataContext);
   const totalItem = basket?.reduce((amount, item) => item.amount + amount, 0) || 0;
+
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handleChange = (e) => {
+    console.log(e);
+    setError(e.error ? e.error.message : '');
+  }
 
   return (
     <LayOut>
@@ -18,7 +27,7 @@ function Payment() {
       <div className='flex'>
         <h3>Delivery Address</h3>
         <div>
-          <div>{user.email}</div>
+          <div>{user?.email}</div>
           <div>123 React Lane</div>
           <div>Chicago, IL</div>
         </div>
@@ -37,9 +46,13 @@ function Payment() {
       {/*card form*/}
       <div className='flex'>
 <h3>Payment methods</h3>
+<div className='payment_card_container'>
 <div>
-<div>
-  <form action=''></form>
+  <form action=''>
+    {error && <small>{error}</small>}
+    <CardElement onChange={handleChange}/>
+    <button>Buy Now</button>
+  </form>
 </div>
 </div>
       </div>
--- Amazon/src/Router.jsx
+++ Amazon/src/Router.jsx
@@ -7,14 +7,19 @@ import Orders from './Pages/Orders/Orders';
 import Cart from './Pages/Cart/Cart';
 import Results from './Pages/Results/Results';
 import ProductDetail from './Pages/ProductDetail/ProductDetail'
+import {Elements} from '@stripe/react-stripe-js';
+import {loadStripe} from '@stripe/stripe-js';
 
+const stripePromise = loadStripe('pk_test_51QWLNoFSD3bz5dFqLl3nH7RnO89IgJichlzQvAynzK95r1ZHsmhZDNhh8oX8DrmcGKMYhUK2MwJsbqX1eb79OGBx007q3lp95u');
 function Routing() {
+
   return (
     <Router>
       <Routes>
         <Route path='/' element={<Landing/>}/>
         <Route path='/auth' element={<Auth/>}/>
-        <Route path='/payments' element={<Payment/>}/>
+        <Route path='/payments' element={
+          <Elements stripe={stripePromise}> <Payment/></Elements>}/>
         <Route path='/category/:categoryName' element={<Results/>} />
         <Route path='/orders' element={<Orders/>}/>
         <Route path='/products/:productId' element={<ProductDetail/>}/>
