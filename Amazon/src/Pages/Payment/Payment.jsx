8248c16caf9b5e1c0def954be35706bafb4caab4
Eyerusalem-Hailemariam <eyerusalem008@gmail.com>
2025-01-25 17:20:18 +0300

    working on payment page

--- Amazon/src/Pages/Payment/Payment.jsx
+++ Amazon/src/Pages/Payment/Payment.jsx
@@ -1,14 +1,42 @@
-import React, {useContext} from 'react'
+import React, {useContext, useState} from 'react'
 import LayOut from '../../Components/LayOut/LayOut'
 import './Payment.css'
 import { DataContext } from '../../Components/DataProvider/DataProvider'
 import ProductCard from '../../Components/Product/ProductCard'
+import {useStripe, useElements, CardElement} from '@stripe/react-stripe-js';
+import CurrencyFormat from '../../Components/CurrencyFormat/CurrencyFormat'
+import { axiosInstance } from '../../Api/axios'
 
 function Payment() {
 
   const [{user, basket}] = useContext(DataContext);
+  const [error, setError] = useState(null);
   const totalItem = basket?.reduce((amount, item) => item.amount + amount, 0) || 0;
+  const total = basket.reduce((amount, item) => item.price * item.amount + amount, 0);
 
+
+  const handleChange = (e) => {
+if (e.error){
+e?.error?.message ? setError(e.error.message) : setError('Please enter a valid card number')
+  }
+}
+
+const handlePayment = async (e) => {
+e.preventDefault();
+try {
+const response = await axiosInstance({
+  method: 'POST',
+  url: `/payment/create?total=${total * 100}`
+})
+console.log(response.data)
+} catch(error){
+  if (error.response){
+    setError(error.response.data.message)
+  } else {
+    setError(error.message)
+
+}
+}}
   return (
     <LayOut>
     <div className='payment_header'>Checkout({totalItem}) items</div>
@@ -18,7 +46,7 @@ function Payment() {
       <div className='flex'>
         <h3>Delivery Address</h3>
         <div>
-          <div>{user.email}</div>
+          <div>{user?.email}</div>
           <div>123 React Lane</div>
           <div>Chicago, IL</div>
         </div>
@@ -36,16 +64,30 @@ function Payment() {
       <hr/>
       {/*card form*/}
       <div className='flex'>
-<h3>Payment methods</h3>
-<div>
-<div>
-  <form action=''></form>
-</div>
-</div>
+        <h3>Payment methods</h3>
+      <div className='payment_card_container'>
+      <div className='payment_details'>
+         <form onSubmit={handlePayment}>
+          {error && <small style={{color :"red"}}>{error}</small>}
+          <CardElement onChange={handleChange}/>
+         {/*price*/}
+      <div className='payment_price'>
+        <div>
+        <span style={{display: 'flex', gap: "10px"}}>
+        <p>Total Order |</p> <CurrencyFormat amount={total} />
+      </span>
+        </div>
+        <button type='submit'>Pay Now</button>
+    </div>
+    </form>
+      </div>
+      
+    </div>
       </div>
     </section>
     </LayOut>
   )
 }
+
 
 export default Payment
--- Amazon/src/Router.jsx
+++ Amazon/src/Router.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from './Pages/Landing/Landing';
 import Auth from './Pages/Auth/Auth';
@@ -6,22 +6,34 @@ import Payment from './Pages/Payment/Payment';
 import Orders from './Pages/Orders/Orders';
 import Cart from './Pages/Cart/Cart';
 import Results from './Pages/Results/Results';
-import ProductDetail from './Pages/ProductDetail/ProductDetail'
+import ProductDetail from './Pages/ProductDetail/ProductDetail';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+
+const stripePromise = loadStripe('pk_test_51QWLNoFSD3bz5dFqLl3nH7RnO89IgJichlzQvAynzK95r1ZHsmhZDNhh8oX8DrmcGKMYhUK2MwJsbqX1eb79OGBx007q3lp95u');
+
+function PaymentWithStripe() {
+  return (
+    <Elements stripe={stripePromise}>
+      <Payment />
+    </Elements>
+  );
+}
 
 function Routing() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Landing/>}/>
-        <Route path='/auth' element={<Auth/>}/>
-        <Route path='/payments' element={<Payment/>}/>
-        <Route path='/category/:categoryName' element={<Results/>} />
-        <Route path='/orders' element={<Orders/>}/>
-        <Route path='/products/:productId' element={<ProductDetail/>}/>
-        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/' element={<Landing />} />
+        <Route path='/auth' element={<Auth />} />
+        <Route path='/payments' element={<PaymentWithStripe />} />
+        <Route path='/category/:categoryName' element={<Results />} />
+        <Route path='/orders' element={<Orders />} />
+        <Route path='/products/:productId' element={<ProductDetail />} />
+        <Route path='/cart' element={<Cart />} />
       </Routes>
     </Router>
   );
 }
 
-export default Routing
+export default Routing;
